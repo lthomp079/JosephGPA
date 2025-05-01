66257e77006e26f6fd7c010a05a9219515c9fce7
lthomp079 <163169113+lthomp079@users.noreply.github.com>
2025-05-01 18:53:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import subprocess
 app = Flask(__name__)
 
 @app.route('/favicon.ico')
-@app.route('/construct')
+@app.route('/hans.html')
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
 def robotAndSitemap():
